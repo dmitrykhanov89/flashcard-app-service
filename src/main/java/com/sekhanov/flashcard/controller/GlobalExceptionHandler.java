262534beae27b3268605550c2daf42dab1cb4125
Dmitry Khanov <dmitry.khanov89@gmail.com>
2025-06-30 08:55:43 +0000

    Merged PR 11: fl-15: Fix PSQLException: ERROR in backend service
    Related work items: #15

--- src/main/java/com/sekhanov/flashcard/controller/GlobalExceptionHandler.java
+++ src/main/java/com/sekhanov/flashcard/controller/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.sekhanov.flashcard.controller;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.postgresql.util.PSQLException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -10,14 +11,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final String SQL_DUPLICATE_KEY_STATE = "23505"; // проверяет на уникальность
+
     @ExceptionHandler(BadCredentialsException.class)
-    private ResponseEntity<Object> exceptionResponse(Exception ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<Object> handleBadCredentials(BadCredentialsException ex) {
+        String message = "Неверный логин или пароль";
+        return new ResponseEntity<>(message, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Object> handleNotFound(EntityNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(PSQLException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolation(PSQLException ex) {
+        if (ex.getSQLState().equals(SQL_DUPLICATE_KEY_STATE)) {
+            String message = "Пользователь с таким логином уже существует";
+            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
 
